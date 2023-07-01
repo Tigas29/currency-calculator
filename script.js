8ddef6f14f118c64efbf177f5d0c624ec1ed2f91
Tiago <tiagoalmeidasantos04@gmail.com>
2023-06-30 23:53:06 -0300

    v.1 sem erros

--- script.js
+++ script.js
@@ -5,26 +5,55 @@ const coinFrom = document.getElementById("coinFrom");
 //   alert("Valores igual, por favor verificar e tentar novamente");
 // }
 
+// function Formatar() {} aqui é usado para formatar o numero
+
 function ErrorTreatment() {
   let CoinForValue = null;
   let CoinFromValue = null;
   let amount = document.getElementById("amount").value;
+
   let result = null;
+  let amountShow = document.getElementById("amountFrom");
+  let imgResultFrom = document.getElementById("imgResultFrom");
+  let imgResultFor = document.getElementById("imgResultFor");
+  let resultFrom = document.getElementById("resultFrom");
+  let resultFor = document.getElementById("resultFor");
+  let amountFor = document.getElementById("amountFor");
+  let coinFromResult = document.getElementById("coinFromResult");
+  let coinForResult = document.getElementById("coinForResult");
 
   if (coinFrom.value.indexOf("Real") >= 0) {
     CoinFromValue = 1;
+    imgResultFrom.src = "./assets/brasil.png";
+    resultFrom.innerText = "Real";
+    coinFromResult.innerText = "R$";
   } else if (coinFrom.value.indexOf("Euro") >= 0) {
     CoinFromValue = 5.23;
+    imgResultFrom.src = "./assets/euro.png";
+    resultFrom.innerText = "Euro";
+    coinFromResult.innerText = "€";
   } else if (coinFrom.value.indexOf("Dolar") >= 0) {
     CoinFromValue = 4;
+    imgResultFrom.src = "./assets/dolar.png";
+    resultFrom.innerText = "Dolar";
+    coinFromResult.innerText = "US$";
   }
 
   if (coinFor.value.indexOf("Real") >= 0) {
     CoinForValue = 1;
+    resultFor.innerText = "Real";
+    imgResultFor.src = "./assets/brasil.png";
+    coinForResult.innerText = "R$";
   } else if (coinFor.value.indexOf("Euro") >= 0) {
     CoinForValue = 5.23;
+    resultFor.innerText = "Euro";
+    imgResultFor.src = "./assets/euro.png";
+    coinForResult.innerText = "€";
   } else if (coinFor.value.indexOf("Dolar") >= 0) {
     CoinForValue = 4;
+    resultFor.innerText = "Dolar";
+    imgResultFor.src = "./assets/dolar.png";
+    coinForResult.innerText = "US$";
   }
 
   if (CoinForValue < CoinFromValue) {
@@ -32,12 +61,12 @@ function ErrorTreatment() {
   } else if (CoinForValue > CoinFromValue) {
     result = amount / CoinForValue;
   }
-  console.log("valor a " + CoinFromValue);
-  console.log("valor b" + CoinForValue);
-  console.log(result);
+  amountShow.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "decimal",
+  }).format(amount);
+  amountFor.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "decimal",
+  }).format(result);
 }
-function Conversion() {}
 
 btn.addEventListener("click", ErrorTreatment);
-
-// se o primeiro for maior que o segundo for mutiplica, senão voce divide
