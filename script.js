8498ad3eb8d7581b4d1ec81ffc6f8a0e50cd61ce
Tiago <tiagoalmeidasantos04@gmail.com>
2023-07-11 21:34:13 -0300

    commit

--- script.js
+++ script.js
@@ -5,13 +5,8 @@ const coinFrom = document.getElementById("coinFrom");
 //   alert("Valores igual, por favor verificar e tentar novamente");
 // }
 
-// function Formatar() {} aqui é usado para formatar o numero
-
+function Formatar() {}
 function ErrorTreatment() {
-  let CoinForValue = null;
-  let CoinFromValue = null;
-  let amount = document.getElementById("amount").value;
-
   let result = null;
   let amountShow = document.getElementById("amountFrom");
   let imgResultFrom = document.getElementById("imgResultFrom");
@@ -56,11 +51,6 @@ function ErrorTreatment() {
     coinForResult.innerText = "US$";
   }
 
-  if (CoinForValue < CoinFromValue) {
-    result = amount * CoinFromValue;
-  } else if (CoinForValue > CoinFromValue) {
-    result = amount / CoinForValue;
-  }
   amountShow.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "decimal",
   }).format(amount);
@@ -68,5 +58,71 @@ function ErrorTreatment() {
     style: "decimal",
   }).format(result);
 }
+function showData() {
+  if (coinFromV.indexOf("Real") >= 0) {
+    imgResultFrom.src = "./assets/brasil.png";
+    resultFrom.innerText = "Real";
+    coinFromResult.innerText = "R$";
+  } else if (coinFromV.indexOf("Euro") >= 0) {
+    imgResultFrom.src = "./assets/euro.png";
+    resultFrom.innerText = "Euro";
+    coinFromResult.innerText = "€";
+  } else if (coinFromV.indexOf("Dolar") >= 0) {
+    imgResultFrom.src = "./assets/dolar.png";
+    resultFrom.innerText = "Dolar";
+    coinFromResult.innerText = "US$";
+  }
+  if (coinForV.indexOf("Real") >= 0) {
+    resultFor.innerText = "Real";
+    imgResultFor.src = "./assets/brasil.png";
+    coinForResult.innerText = "R$";
+  } else if (coinForV.indexOf("Euro") >= 0) {
+    resultFor.innerText = "Euro";
+    imgResultFor.src = "./assets/euro.png";
+    coinForResult.innerText = "€";
+  } else if (coinForV.indexOf("Dolar") >= 0) {
+    resultFor.innerText = "Dolar";
+    imgResultFor.src = "./assets/dolar.png";
+    coinForResult.innerText = "US$";
+  }
+}
+function calculate(valueFor, ValueFrom) {
+  let result;
+  let amount = document.getElementById("amount").value;
 
-btn.addEventListener("click", ErrorTreatment);
+  if (valueFor < ValueFrom) {
+    result = amount * ValueFrom;
+  } else if (valueFor > ValueFrom) {
+    result = amount / valueFor;
+  }
+
+  console.log(result);
+}
+function getDatas(coinForV) {
+  let CoinForValue = null;
+  let CoinFromValue = null;
+  if (coinFromV.indexOf("Real") >= 0) {
+    CoinFromValue = 1;
+  } else if (coinFromV.indexOf("Euro") >= 0) {
+    CoinFromValue = 5.23;
+  } else if (coinFromV.indexOf("Dolar") >= 0) {
+    CoinFromValue = 4;
+  }
+
+  if (coinForV.indexOf("Real") >= 0) {
+    CoinForValue = 1;
+  } else if (coinForV.indexOf("Euro") >= 0) {
+    CoinForValue = 5.23;
+  } else if (coinForV.indexOf("Dolar") >= 0) {
+    CoinForValue = 4;
+  }
+  calculate(coinForValue, coinFromValue);
+}
+
+function ClicketButton() {
+  const coinFor = document.getElementById("coinFor");
+  const coinFrom = document.getElementById("coinFrom");
+  getDatas(coinFor, coinFrom);
+}
+
+btn.addEventListener("click", ClicketButton);
